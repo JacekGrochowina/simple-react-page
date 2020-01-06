7638c822b83d03ecc3c2b0c71a263d46dda46693
Jacek Grochowina <jacek99@vp.pl>
2020-01-06 15:23:47 +0100

    display route path

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -1,8 +1,35 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+import '../styles/Footer.css';
 
 const Footer = () => {
     return (
-        <div>Footer</div>
+        <div>
+            <p>Stopka</p>
+            <Route path="/" exact render={(props) => (
+                <p className="src">strona główna</p>
+            )
+            } />
+
+            <Route path="/:page" exact render={(props) => (
+                <>
+                    {/* <p className="src">{props.match.params.page}</p> */}
+                    <p className="src">{props.match.url}</p>
+                    {/* <p className="src">{props.match.path}</p> */}
+                </>
+            )
+            } />
+
+            <Route path="/:page/:idProduct" exact render={(props) => (
+                <>
+                    {/* <p className="src">{props.match.params.idProduct}</p> */}
+                    <p className="src">{props.match.url}</p>
+                    {/* <p className="src">{props.match.path}</p> */}
+                </>
+            )
+            } />
+
+        </div>
     )
 }
 
